74d30ab98520182d06f392266c3416da8adc9fe9
ibrahimakpinar <ibrahim.akpinar@outlook.com.tr>
2023-02-25 15:52:55 +0300

    feat: new project

---
+++ Password/Password/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Password
+//
+//  Created by ibrahim AKPINAR on 25.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return false
+    }
+}
+
